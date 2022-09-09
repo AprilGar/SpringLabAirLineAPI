60c8ad420f12b278f585f3e6bf0499f01da0beae
April Garlington <april.garlington@hotmail.co.uk>
2022-09-09 11:34:14 +0100

    ToDo list made

--- src/main/java/com/example/SpringAirLineLabAPI/controllers/FlightController.java
+++ src/main/java/com/example/SpringAirLineLabAPI/controllers/FlightController.java
@@ -1,4 +1,13 @@
 package com.example.SpringAirLineLabAPI.controllers;
 
 public class FlightController {
+
+    //TODO
+    //Add details of a new flight
+    //Display all available flights
+    //Display details of a specific flight
+    //Book a passenger on to a flight
+    //Cancel a flight
+
+
 }
--- src/main/java/com/example/SpringAirLineLabAPI/controllers/PassengerController.java
+++ src/main/java/com/example/SpringAirLineLabAPI/controllers/PassengerController.java
@@ -1,4 +1,10 @@
 package com.example.SpringAirLineLabAPI.controllers;
 
 public class PassengerController {
+
+    //TODO
+    //Add a new passenger
+    //Display details of all passengers
+    //Display details of a specific passenger
+
 }
